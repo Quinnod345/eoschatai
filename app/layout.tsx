bf78e1ac56c6fc5d97d93ee180316b5882049bea
Quinn O'Donnell <odonnellquinn34@gmail.com>
2025-05-16 08:23:16 -0400

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { UISettingsProvider } from '@/components/ui-settings-provider';
+import { Analytics } from '@vercel/analytics/next';
 
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
@@ -113,6 +114,7 @@ export default async function RootLayout({
           <SessionProvider>
             <UISettingsProvider>{children}</UISettingsProvider>
           </SessionProvider>
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
