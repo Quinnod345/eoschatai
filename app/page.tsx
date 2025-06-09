b0c510aaa8be130fd9f4e31fe01521c4d89be06f
Quinn O'Donnell <odonnellquinn34@gmail.com>
2025-06-09 00:29:38 -0400

    page.ysx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-import { auth } from './(auth)/auth';
-import HomeClient from './HomeClient';
-import ClientRedirect from '@/components/client-redirect';
+import { redirect } from 'next/navigation';
 
-export default async function Home() {
-  const session = await auth();
-
-  // If user is already logged in, redirect to chat using client-side redirect
-  if (session) {
-    return <ClientRedirect path="/chat" />;
-  }
-
-  return <HomeClient />;
+export default function Home() {
+  redirect('/login');
 }
